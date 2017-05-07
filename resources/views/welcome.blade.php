f0c69a5e85528bba37ddb0460d231f398c06ab01
venkat24 <venkatramansrikanth@outlook.com>
2017-05-08 01:28:58 +0530

    Add Contact thumbnails and improve RR animation

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -104,7 +104,7 @@
 			  		
                             <div>
                                     <img class="reel" src="images/reel.png" width="200px" height="200px">
-                                    <!--<img class="reel2" src="images/reel.png" width="200px" height="200px">-->
+                                    <img class="reel2" src="images/reel2.png" width="200px" height="200px">
                             </div>
                             <br>
 			  				<h3 class="rolling-font animate-intro">Festember Presents</h3>
@@ -115,6 +115,9 @@
 				  			<div class="more animate-intro">
 				  				<a class="smoothscroll button stroke" href="#about">
 				  					Learn More
+				  				</a>&nbsp&nbsp&nbsp&nbsp
+				  				<a class="smoothscroll button stroke" href="/register">
+                                    Register Now
 				  				</a>
 				  			</div>							
 
@@ -512,91 +515,11 @@
    	</div> <!-- end section-intro -->
 
    	<div class="row contact-content">
-
-   		<div class="col-seven tab-full animate-this">
-
-   			<h5>Send Us A Message</h5>
-
-            <!-- form -->
-            <form name="contactForm" id="contactForm" method="post">     			
-
-               <div class="form-field">
- 					   <input name="contactName" type="text" id="contactName" placeholder="Name" value="" minlength="2" required="">
-               </div>
-
-               <div class="row">
-                 	<div class="col-six tab-full">
-                 		<div class="form-field">
-                 			<input name="contactEmail" type="email" id="contactEmail" placeholder="Email" value="" required="">
-                 		</div>		      			   
-		            </div>
-	            	<div class="col-six tab-full">	            
-	            		<div class="form-field">
-	            			<input name="contactSubject" type="text" id="contactSubject" placeholder="Subject" value="">
-	                  </div>		     				   
-		            </div>
-               </div>
-                                         
-               <div class="form-field">
-	              	<textarea name="contactMessage" id="contactMessage" placeholder="message" rows="10" cols="50" required=""></textarea>
-	            </div> 
-
-               <div class="form-field">
-                  <button class="submitform">Submit</button>
-
-                  <div id="submit-loader">
-                     <div class="text-loader">Sending...</div>                             
-       			      <div class="s-loader">
-							  	<div class="bounce1"></div>
-							  	<div class="bounce2"></div>
-							  	<div class="bounce3"></div>
-							</div>
-						</div>
-               </div>
-
-      		</form> <!-- end form -->
-
-            <!-- contact-warning -->
-            <div id="message-warning"></div> 
-
-            <!-- contact-success -->
-      		<div id="message-success">
-               <i class="fa fa-check"></i>Your message was sent, thank you!<br>
-      		</div>
-
-         </div> <!-- end col-seven --> 
-
-         <div class="col-four tab-full contact-info end animate-this">
-
-         	<h5>Contact Information</h5>
-
-         	<div class="cinfo">
-	   			<h6>Where to Find Us</h6>
-	   			<p>
-	            	1600 Amphitheatre Parkway<br>
-	            	Mountain View, CA<br>
-	            	94043 US
-	            </p>
-	   		</div> <!-- end cinfo -->
-
-	   		<div class="cinfo">
-	   			<h6>Email Us At</h6>
-	   			<p>
-	   				someone@infinitysite.com<br>
-				   	info@infinitysite.com			     
-				   </p>
-	   		</div> <!-- end cinfo -->
-
-	   		<div class="cinfo">
-	   			<h6>Call Us At</h6>
-	   			<p>
-	   				Phone: (+63) 555 1212<br>
-				   	Mobile: (+63) 555 0100<br>
-				     	Fax: (+63) 555 0101
-				   </p>
-	   		</div>
-
-         </div> <!-- end cinfo --> 
+    
+        <div class="profile-container"><img src="images/profile.png" class="profile-img"><p>Deepan Thiruppathy</p></div>
+        <div class="profile-container"><img src="images/profile.png" class="profile-img"><p>Deepan Thiruppathy</p></div>
+        <div class="profile-container"><img src="images/profile.png" class="profile-img"><p>Deepan Thiruppathy</p></div>
+        <div class="profile-container"><img src="images/profile.png" class="profile-img"><p>Deepan Thiruppathy</p></div>
 
    	</div> <!-- end row contact-content -->
 		
@@ -665,8 +588,8 @@
 
       		<div class="col-twelve">
 	      		<div class="copyright">
-		         	<span>© Copyright Infinity 2016.</span> 
-		         	<span>Design by <a href="http://www.styleshout.com/">styleshout</a></span>		         	
+		         	<span>© Festember 2017</span> 
+                    <span>Made with ♥ by <a href="http://deltaforce.club" target="_blank">Delta Force</a></span>
 		         </div>		               
 	      	</div>
 
