5ff2fbf25dbe88bfc0875b37423201aa84f98fc0
venkat24 <venkatramansrikanth@outlook.com>
2017-05-14 23:52:15 +0530

    Add judges names and pictures

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -249,6 +249,65 @@
 
    </section> <!-- end services -->
 
+   <!-- Testimonials Section
+   ================================================== -->
+   <section id="testimonials">
+
+   	<div class="row">
+   		<div class="col-twelve">
+   			<h2 class="animate-this">Out Esteemed Panel of Judges</h2>
+   		</div>   		
+   	</div>   	
+
+      <div class="row flex-container">
+    
+         <div id="testimonial-slider" class="flex-slider animate-this">
+
+            <ul class="slides">
+
+               <li>
+                  <div class="testimonial-author">
+                    	<img src="images/avatars/user-02.jpg" alt="Author image">
+                    	<div class="author-info">
+                    		R Velraj
+                    		<span class="position">Director</span>
+                    	</div>
+                  </div>
+
+                  <p></p> 
+             	</li> <!-- end slide -->
+
+               <li>
+               	<div class="testimonial-author">
+                    	<img src="images/avatars/user-03.jpg" alt="Author image">
+                    	<div class="author-info">
+                    		Sreekar Prasad
+                    		<span>Film Editor</span>
+                    	</div>
+                  </div>                                         
+
+                  <p></p>
+               </li> <!-- end slide -->
+
+               <li>
+               	<div class="testimonial-author">
+                    	<img src="images/avatars/user-04.jpg" alt="Author image">
+                    	<div class="author-info">
+                    		DRK Kiran
+                    		<span>Actor / Art Director</span>
+                    	</di5>
+                  </div>                                         
+
+                  <p></p>
+               </li> <!-- end slide -->
+
+            </ul> <!-- end slides -->
+
+         </div> <!-- end testimonial-slider -->         
+        
+      </div> <!-- end flex-container -->
+
+   </section> <!-- end testimonials -->
 
    <!-- portfolio
    ================================================== -->
@@ -418,63 +477,6 @@
    </section>  <!-- end portfolio -->
 
 
-   <!-- Testimonials Section
-   ================================================== -->
-   <section id="testimonials">
-
-   	<div class="row">
-   		<div class="col-twelve">
-   			<h2 class="animate-this">What They Say About Us.</h2>
-   		</div>   		
-   	</div>   	
-
-      <div class="row flex-container">
-    
-         <div id="testimonial-slider" class="flex-slider animate-this">
-
-            <ul class="slides">
-
-               <li>
-                  <p>
-                  Your work is going to fill a large part of your life, and the only way to be truly satisfied is
-                  to do what you believe is great work. And the only way to do great work is to love what you do.
-                  If you haven't found it yet, keep looking. Don't settle. As with all matters of the heart, you'll know when you find it.
-                  </p> 
-
-                  <div class="testimonial-author">
-                    	<img src="images/avatars/user-02.jpg" alt="Author image">
-                    	<div class="author-info">
-                    		Steve Jobs
-                    		<span class="position">CEO, Apple.</span>
-                    	</div>
-                  </div>                 
-             	</li> <!-- end slide -->
-
-               <li>
-                  <p>
-                  This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet.
-                  Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem
-                  nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet mauris.    
-                  </p>
-
-               	<div class="testimonial-author">
-                    	<img src="images/avatars/user-03.jpg" alt="Author image">
-                    	<div class="author-info">
-                    		John Doe
-                    		<span>CEO, ABC Corp.</span>
-                    	</div>
-                  </div>                                         
-               </li> <!-- end slide -->
-
-            </ul> <!-- end slides -->
-
-         </div> <!-- end testimonial-slider -->         
-        
-      </div> <!-- end flex-container -->
-
-   </section> <!-- end testimonials -->
-
-
 	<!-- contact
    ================================================== -->
    <section id="contact">
@@ -508,12 +510,6 @@
 	        <br /><strong>Varshini Bhaskar</strong>
 	        <br />Media Head, Festember 2017
 	        <br />for Media collaborations
-        </div>
-
-        <div class="profile-container"><img src="images/avatars/Shriya_Narayanan.jpg" class="profile-img">
-	        <br /><strong>Shriya Narayanan</strong>
-	        <br />Marketing Head, Festember 2017
-	        <br />for Sponsorship queries
         </div>
 
         <div class="profile-container"><img src="images/avatars/Aakash_Bhatt.jpg" class="profile-img">
